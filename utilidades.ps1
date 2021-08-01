4921c1c4deef4693ca118d8b288b6d783c3def06
rdev32 <78289548+rdev32@users.noreply.github.com>
2021-08-01 14:54:51 -0500

    v1.0

---
+++ utilidades.ps1
@@ -0,0 +1,50 @@
+﻿# Si este es tu primer escript, abre un powershell como admin y escribe : 'Set-ExecutionPolicy Bypass'
+# Autor : https://github.com/rdev32
+$Host.UI.RawUI.WindowTitle = "Utilidades Arcanas";
+$run = $false;
+if ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
+    while (!$run) {
+        Clear-Host;
+        Write-Host "Opciones";
+        Write-Host "1 - Activar Windows 10 Home/Pro";
+        Write-Host "2 - Remover Objetos 3D";
+        Write-Host "3 - Salir";
+        $cmd = Read-Host "Escribe un numero listado ";
+        if ($cmd -eq "1") {
+            Clear-Host;
+            Write-Host "Detectado:" (Get-WmiObject -class Win32_OperatingSystem).Caption;
+            Write-Host "Activando...";
+            if ("Microsoft Windows 10 Pro" -eq (Get-WmiObject -class Win32_OperatingSystem).Caption) {
+                slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX;
+                slmgr.vbs /skms s8.now.im;
+                slmgr.vbs /ato;
+                Write-Host "Operacion exitosa!";
+            }
+
+            if ("Microsoft Windows 10 Home" -eq (Get-WmiObject -class Win32_OperatingSystem).Caption) {
+                slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99;
+                slmgr.vbs /skms s8.now.im;
+                slmgr.vbs /ato;
+                Write-Host "Operacion exitosa!";
+            } 
+            Read-Host -Prompt "Presiona cualquier tecla para volver al menu";
+        }
+        if ($cmd -eq "2") {
+            Clear-Host;
+            Write-Host "Eliminando Objetos 3D...";
+            $DAT = 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}';
+            $INF = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}';
+            if (Test-Path $DAT) {
+                if (Test-Path $INF) {
+                    Remove-Item $DAT;
+                    Remove-Item $INF;
+                    Write-Host "Se elimino con exito.";
+                }
+            } else {
+                Write-Host "Ya habias borrado esto antes?";
+                Read-Host -Prompt "Presiona cualquier tecla para volver al menu";
+            }
+        }
+        if ($cmd -eq "3") { $run = $true; }
+    }
+} else { Write-Host "Debes ejecutar esto con permisos de administrador." }
